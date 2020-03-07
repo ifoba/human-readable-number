8cf71ce68a69b8f938f432417192e9014d7c33e1
Maxim Ryabov <foba1995@gmail.com>
2020-03-07 22:25:41 +0400

    feat: get result

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 module.exports = function toReadable (number) {
+  let arrOne = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+  let arrTen = ['twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+//999
+  let result = [];
+  if (!number) result.push(arrOne[number]);
+  if (number>=100){
+      result.push(arrOne[Math.floor(number/100)]);
+      result.push('hundred');
+      number %= 100;
+  }
+  //99
+
+  if (number>=20) {
+      result.push(arrTen[Math.floor(number/10)-2]);
+      number %= 10;
+            
+  }
+  //9
+
+  if (number>=0 && !number%10==0) {
+      result.push(arrOne[number]);
+  }
+
   
+
+  return result.join(' ');
+
 }
